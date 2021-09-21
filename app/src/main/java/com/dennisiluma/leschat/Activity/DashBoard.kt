2f19b980817565320ab7d5f3df7a4d2de8d64763
dennisluma <ilumadennis20@gmail.com>
2021-09-21 11:10:07 +0100

    feature upload image to firebase storage

---
+++ app/src/main/java/com/dennisiluma/leschat/Activity/DashBoard.kt
@@ -0,0 +1,12 @@
+package com.dennisiluma.leschat.Activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.dennisiluma.leschat.R
+
+class DashBoard : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_dash_board)
+    }
+}
--- app/src/main/java/com/dennisiluma/leschat/Fragment/GetUserData.kt
+++ app/src/main/java/com/dennisiluma/leschat/Fragment/GetUserData.kt
@@ -1,15 +1,38 @@
 package com.dennisiluma.leschat.Fragment
 
+import android.Manifest
+import android.app.Activity
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.net.Uri
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.dennisiluma.leschat.R
+import android.widget.Toast
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import com.dennisiluma.leschat.Activity.DashBoard
+import com.dennisiluma.leschat.Constants.AppConstants
 import com.dennisiluma.leschat.databinding.FragmentGetUserDataBinding
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.storage.FirebaseStorage
+import com.google.firebase.storage.StorageReference
+import com.theartofdev.edmodo.cropper.CropImage
+import com.theartofdev.edmodo.cropper.CropImageView
 
 
 class GetUserData : Fragment() {
+    private var image: Uri? = null
+    private lateinit var username: String
+    private lateinit var status: String
+    private lateinit var imageUrl: String
+    private var databaseReference: DatabaseReference? = null
+    private var firebaseAuth: FirebaseAuth? = null
+    private var storageReference: StorageReference? = null
 
     private var _binding: FragmentGetUserDataBinding? = null
     private val binding get() = _binding!!
@@ -24,6 +47,105 @@ class GetUserData : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        firebaseAuth = FirebaseAuth.getInstance()
+        databaseReference = FirebaseDatabase.getInstance().getReference("Users")
+        storageReference = FirebaseStorage.getInstance().reference
+
+        binding.btnDataDone.setOnClickListener {
+            if (checkData()) {
+                image?.let { it1 -> uploadData(username, status, it1) }
+            }
+        }
+        binding.imgPickImage.setOnClickListener {
+            if (checkStoragePermission())
+                pickImage()
+            else storageRequestPermission()
+        }
+    }
+
+    private fun checkData(): Boolean {
+        username = binding.edtUserName.text.toString().trim()
+        status = binding.edtUserStatus.text.toString().trim()
+
+        return if (username.isEmpty()) {
+            binding.edtUserName.error = "Filed is required"
+            false
+        } else if (status.isEmpty()) {
+            binding.edtUserStatus.error = "Filed is required"
+            false
+        } else if (image == null) {
+            Toast.makeText(context, "Image required", Toast.LENGTH_SHORT).show()
+            false
+        } else true
+    }
+
+    private fun checkStoragePermission(): Boolean {
+        return ContextCompat.checkSelfPermission(
+            requireContext(),
+            Manifest.permission.READ_EXTERNAL_STORAGE
+        ) == PackageManager.PERMISSION_GRANTED
+    }
+
+    private fun storageRequestPermission() = ActivityCompat.requestPermissions(
+        requireActivity(),
+        arrayOf(
+            Manifest.permission.READ_EXTERNAL_STORAGE,
+            Manifest.permission.WRITE_EXTERNAL_STORAGE
+        ), 1000
+    )
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        when (requestCode) {
+            1000 ->
+                if (grantResults[0] == PackageManager.PERMISSION_GRANTED)
+                    pickImage()
+                else Toast.makeText(context, "Storage permission denied", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        when (requestCode) {
+
+            CropImage.CROP_IMAGE_ACTIVITY_REQUEST_CODE -> {
+                val result = CropImage.getActivityResult(data)
+                if (resultCode == Activity.RESULT_OK) {
+                    image = result.uri
+                    binding.imgUser.setImageURI(image)
+                }
+            }
+        }
+    }
+
+    private fun uploadData(name: String, status: String, image: Uri) = kotlin.run {
+        storageReference!!.child(firebaseAuth!!.uid + AppConstants.PATH).putFile(image)
+            .addOnSuccessListener {
+                val task = it.storage.downloadUrl
+                task.addOnCompleteListener { uri ->
+                    imageUrl = uri.result.toString()
+                    val map = mapOf(
+                        "name" to name,
+                        "status" to status,
+                        "image" to imageUrl
+                    )
+                    databaseReference!!.child(firebaseAuth!!.uid!!).updateChildren(map)
+
+                    startActivity(Intent(context, DashBoard::class.java))
+                    requireActivity().finish()
+                }
+            }
+    }
+
+
+    private fun pickImage() {
+        CropImage.activity()
+            .setCropShape(CropImageView.CropShape.OVAL)
+            .start(requireContext(), this)
     }
 
     override fun onDestroy() {
